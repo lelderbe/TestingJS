be8ba59b5864ac1e348783a96365648b5ebdd02a
lelderbe <fvdc@ya.ru>
2021-07-10 16:27:53 +0300

    life hacks

---
+++ life_hacks.js
@@ -0,0 +1,30 @@
+function ex_life_hacks() {
+	console.log('\n------------ life hacks --------------');
+
+	value = 100;
+
+	console.log('value :', value);
+	console.log('convert to boolean: !!value :', !!value);
+	console.log('!!"" :', !!'');
+	console.log('!!" " :', !!' ');
+	console.log('value.toString(2) :', value.toString(2));
+	console.log('undefined :', undefined);
+	
+	let max_witdh;
+	prefs = {}
+	//prefs.max_witdh = 100;
+	
+	max = max_witdh && prefs.max_witdh && 500;
+	console.log(max);
+	max = max_witdh || prefs.max_witdh || 500;
+	console.log(max);
+	
+	console.log('max = max_witdh || prefs.max_witdh || 500 :', max);
+
+	console.log('this :', this);
+}
+
+ex_life_hacks();
+
+console.log('this :', this);
+
