d266c13fb25a20a1e36758f9a39bf0f80f515af4
lelderbe <fvdc@ya.ru>
2022-01-04 22:05:08 +0300

    functions: wrappers

--- functions/wrapper/solution.js
+++ functions/wrapper/solution.js
@@ -80,4 +80,68 @@ function ex03() {
 
 }
 
-ex03();
+// ex03();
+
+function ex04() {
+	console.log('\n------------ Заимствование метода ---------------');
+	function f1() {
+		console.log(arguments);
+		console.log([].join.call(arguments, '-'));
+	}
+	f1(1, 2, 3);
+}
+
+// ex04();
+
+function ex05() {
+	console.log('\n------------ Task: Декоратор-шпион ---------------');
+	function spy(f) {
+		function wrapper(...args) {
+			wrapper.calls.push(args);
+			// return f(...args);
+			// return f.apply(this, ...args);
+			return f.apply(this, arguments);
+		}
+		wrapper.calls = [];
+		return wrapper;
+	}
+
+	function work(a, b) {
+		console.log(a + b); // произвольная функция или метод
+	}
+
+	work = spy(work);
+
+	work(1, 2); // 3
+	work(4, 5); // 9
+
+	console.log(work.calls);
+	for (let args of work.calls) {
+		console.log('call:' + args.join()); // "call:1,2", "call:4,5"
+	}
+}
+
+// ex05();
+
+function ex06() {
+	console.log('\n------------ Task: Задерживающий декоратор ---------------');
+
+	function f(x) {
+		console.log(x);
+	}
+
+	function delay(f, ms) {
+		return function() {
+			setTimeout(() => f.apply(this, arguments), ms);
+		}
+	}
+
+	// создаём обёртки
+	let f1000 = delay(f, 1000);
+	let f1500 = delay(f, 1500);
+
+	f1000("test"); // показывает "test" после 1000 мс
+	f1500("test"); // показывает "test" после 1500 мс
+}
+
+ex06();
