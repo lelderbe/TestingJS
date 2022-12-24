90c2f67591d3f561e270b4356bccd0902152cd55
lelderbe <fvdc@ya.ru>
2022-12-24 21:30:54 +0300

    function-constructor

---
+++ functions/constructor/ex00.js
@@ -0,0 +1,13 @@
+function Range(from, to) {
+  if (!new.target) return new Range(from, to);
+  this.from = from;
+  this.to = to;
+}
+
+const a = new Range(1, 3);
+const b = new Range(0, 2);
+console.log(a, b);
+
+const a2 = Range(1, 3);
+const b2 = new Range(0, 2);
+console.log(a2, b2);
