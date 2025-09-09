c4500f83ca973c60d5afe007cc8cb1cc26104d53
lelderbe <fvdc@ya.ru>
2025-09-09 22:53:07 +0300

    feat: add test async-await

--- async/async00.js
+++ async/async00.js
@@ -1,8 +1,14 @@
+async function timeout(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
 const logDelayedMessages = async () => {
-	await timeout(1000);
-	console.log('Hi after 1 second');
-	await timeout(1000);
-	console.log('Hi after 2 seconds');
+    console.log('before first timeout');
+    await timeout(1000);
+    console.log('Hi after 1 second');
+    await timeout(1000);
+    console.log('Hi after 2 seconds');
 };
 
 logDelayedMessages();
+console.log(1);
